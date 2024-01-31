5da51ad1f23ed87aed8942d690f5e616746cdf48
lcensies <lcensies@proton.me>
2024-01-31 11:31:26 +0300

    Add metrics for cache

--- pkg/metrics/stats.go
+++ pkg/metrics/stats.go
@@ -1,10 +1,14 @@
 package metrics
 
 import (
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 
+	"github.com/aquasecurity/tracee/pkg/config"
 	"github.com/aquasecurity/tracee/pkg/counter"
 	"github.com/aquasecurity/tracee/pkg/errfmt"
+	"github.com/aquasecurity/tracee/pkg/logger"
 )
 
 // When updating this struct, please make sure to update the relevant exporting functions
@@ -20,14 +24,15 @@ type Stats struct {
 	LostBPFLogsCount counter.Counter
 }
 
+type MetricBuilder func() prometheus.Collector
+
 // Register Stats to prometheus metrics exporter
-func (stats *Stats) RegisterPrometheus() error {
+func (stats *Stats) RegisterPrometheus(traceeConfig config.Config) error {
 	err := prometheus.Register(prometheus.NewCounterFunc(prometheus.CounterOpts{
 		Namespace: "tracee_ebpf",
 		Name:      "events_total",
 		Help:      "events collected by tracee-ebpf",
 	}, func() float64 { return float64(stats.EventCount.Get()) }))
-
 	if err != nil {
 		return errfmt.WrapError(err)
 	}
@@ -91,6 +96,62 @@ func (stats *Stats) RegisterPrometheus() error {
 	if err != nil {
 		return errfmt.WrapError(err)
 	}
+
+	// cachedEventsCounter := func() prometheus.Collector {
+	// }
+
+	err = prometheus.Register(prometheus.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "tracee_ebpf",
+		Name:      "events_cached",
+		Help:      "number of cached events",
+	}, func() float64 {
+		// size := float64(r.TraceeConfig.Cache.Size())
+		// size := 34.0
+		cache := traceeConfig.Cache
+		if cache == nil {
+			logger.Errorw("Cache is nil. Returning 0.0 size")
+			return 0.0
+		} else {
+			logger.Infow("Cache size is ", strconv.Itoa(cache.Size()))
+			return float64(cache.Size())
+		}
+
+		// return size
+	}))
+
+	if err != nil {
+		return errfmt.WrapError(err)
+	}
+
+	// TODO: change to ConstMetric
+	err = prometheus.Register(prometheus.NewCounterFunc(prometheus.CounterOpts{
+		Namespace: "tracee_ebpf",
+		Name:      "cache_capacity",
+		Help:      "capacity of the cache in events",
+	}, func() float64 {
+		// size := float64(r.TraceeConfig.Cache.Capacity())
+		cache := traceeConfig.Cache
+		if cache == nil {
+			return 0.0
+		} else {
+			return float64(cache.Capacity())
+		}
+		// size := 34.0
+		// return size
+	}))
+
+	if err != nil {
+		return errfmt.WrapError(err)
+	}
+
+	// for _, builder := range builders {
+	// 	err = prometheus.Register(builder())
+	//
+	// 	if err != nil {
+	// 		return errfmt.WrapError(err)
+	// 	}
+	//
+	// }
 
 	err = prometheus.Register(prometheus.NewCounterFunc(prometheus.CounterOpts{
 		Namespace: "tracee_ebpf",
