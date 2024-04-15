4699ac9b7277d0a28d3bfeffd71219f267b6af55
lcensies <lcensies@proton.me>
2024-04-15 19:13:10 +0300

    use default events set in dos_test.sh

--- tests/perftests/dos_test.sh
+++ tests/perftests/dos_test.sh
@@ -2,7 +2,7 @@
 
 PROMETHEUS_ADDR=http://localhost:9090
 
-DOS_SLEEP_DURATION_SEC=${DOS_SLEEP_DURATION_SEC:-0.001}
+DOS_SLEEP_DURATION_SEC=${DOS_SLEEP_DURATION_SEC:-0.018}
 DOS_DURATION_SEC=60
 DOS_CMD=/app/dos
 DOS_N_FAKE_COMMANDS="2000"
@@ -12,7 +12,7 @@ DOS_CPU_LIMIT=${DOS_CPU_LIMIT:-"1"}
 # DOS_CMD="while true; do cat /etc/passwd && date && sleep 0.2; done"
 TRACEE_ROOT=$(git rev-parse --show-toplevel)
 TRACEE_CACHE_TYPE=${TRACEE_CACHE_TYPE:-mem}
-TRACEE_MEM_CACHE_SIZE=${TRACEE_MEM_CACHE_SIZE:-4096}
+TRACEE_MEM_CACHE_SIZE=${TRACEE_MEM_CACHE_SIZE:-2048}
 TRACEE_DISK_CACHE_SIZE=${TRACEE_DISK_CACHE_SIZE:-16384}
 TRACEE_PERF_BUFFER_SIZE=${TRACEE_PERF_BUFFER_SIZE:-1024}
 TRACEE_LISTEN_ADDR=http:/localhost:3366
@@ -48,9 +48,13 @@ clear_prometheus() {
 	# curl -X POST -g "$PROMETHEUS_ADDR/api/v1/admin/tsdb/delete_series?match[]=tracee_ebpf_lostevents_total"
 	# curl -X POST -g "$PROMETHEUS_ADDR/api/v1/admin/tsdb/delete_series?match[]=tracee_ebpf_events_total"
 
+	perf_compose="$TRACEE_ROOT/performance/dashboard/docker-compose.yml"
+	docker-compose -f "$perf_compose" down
+	docker volume rm dashboard_prometheus_data 2>/dev/null || :
+
 	# Set the match[] argument to "{}" to match all metrics
-	MATCHERS='{}'
-	curl -X POST "$PROMETHEUS_ADDR/api/v1/admin/tsdb/delete_series" -H "Content-Type: application/json" --data "{\"match[]\":[\"$MATCHERS\"]}"
+	# MATCHERS='{}'
+	# curl -X POST "$PROMETHEUS_ADDR/api/v1/admin/tsdb/delete_series" -H "Content-Type: application/json" --data "{\"match[]\":[\"$MATCHERS\"]}"
 
 }
 
@@ -77,16 +81,15 @@ teardown() {
 start_tracee() {
 	echo Starting tracee container
 
-	# TODO: fix logging to file
-	# --log-opt max-size=400m
-	# --scope container
-
 	# docker run --name tracee -d --rm --pid=host --cgroupns=host --privileged -e TRACEE_EXE=$TRACEE_EXE -v /run/docker.sock:/var/run/docker.sock \
 	# 	-v /var/run:/var/run:ro -v /tmp/tracee:/tmp/tracee -v /boot:/boot -v $(pwd)/tracee:/etc/tracee -v /etc/os-release:/etc/os-release-host:ro -e "${TRACEE_EVENTS}" \
 	# 	-p 3366:3366 tracee:latest --healthz=true --metrics --output json --output out-file:${TRACEE_LOG_FILE} "${tracee_cache_params[@]}" \
 	# 	--perf-buffer-size="${TRACEE_PERF_BUFFER_SIZE}"
 
-	docker run --name tracee -e TRACEE_EXE=${TRACEE_EXE} -d --rm --pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -v /boot:/boot -v /var/run:/var/run:ro -v /tmp/tracee:/tmp/tracee -p 3366:3366 tracee:latest --metrics --healthz=true --output json --output out-file:/tmp/tracee/tracee.log -e security_file_open #open,openat
+	# --log debug
+	# --log debug --log file:/tmp/tracee.log
+	docker run --name tracee -e TRACEE_EXE=/tracee/tracee-ebpf -d --rm --pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -v /boot:/boot -v /var/run:/var/run:ro -v /tmp/tracee:/tmp/tracee -p 3366:3366 tracee:latest --metrics --healthz=true --output out-file:/tmp/tracee/tracee.log --cache cache-type=mem --cache mem-cache-size=${TRACEE_MEM_CACHE_SIZE} -e security_file_open,creat,chmod,fchmod,chown,fchown,lchown,ptrace,setuid,setgid,setpgid,setsid,setreuid,setregid,setresuid,setresgid,setfsuid,setfsgid,init_module,fchownat,fchmodat,setns,process_vm_readv,process_vm_writev,finit_module,memfd_create,move_mount,sched_process_exec,security_inode_unlink,security_socket_connect,security_socket_accept,security_socket_bind,security_sb_mount,net_packet_icmp,net_packet_icmpv6,net_packet_dns_request,net_packet_dns_response,net_packet_http_request,net_packet_http_response
+	# -o option:sort-events # --proctree source=events
 
 	echo Waiting for tracee to start
 	while
@@ -124,16 +127,13 @@ _main() {
 
 	# [[ $call_teardown -eq 1 ]] && teardown
 
+	teardown
 	start_prometheus
-	clear_prometheus
-
 	tracee_is_running || start_tracee
 
 	run_dos
 
 	wait_tracee_sink
-
-	docker stop tracee
 
 	if [ "$TRACEE_BENCHMARK_OUTPUT_FILE" != "" ]; then
 		benchmark_dir=${TRACEE_BENCHMARK_OUTPUT_FILE%/*}
