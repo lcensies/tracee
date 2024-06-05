f99afd8f9a7151cddebb938f678deeb6cf7b7633
lcensies <lcensies@proton.me>
2024-06-05 07:30:46 +0300

    add get_top_io to label_experiment.sh

--- tests/perftests/label_experiment.sh
+++ tests/perftests/label_experiment.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_DIR="$(cd ${0%/*} && pwd -P)"
 source "$SCRIPT_DIR/.env"
+source "$SCRIPT_DIR/.local.env"
 
-python3 "$SCRIPT_DIR/analyze_ev.py"
+python3 "${SCRIPT_DIR}/analyze_ev.py"
+python3 "${SCRIPT_DIR}/get_top_io.py"
 
 experiment_dir="$EXPERIMENTS_DIR/$1"
 
